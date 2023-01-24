e50da0fa286ef02597c40a10a0c678a10afff75d
ThalyaCuin <thalyacuin@gmail.com>
2023-01-23 23:50:23 -0300

    aula12

--- sketch.js
+++ sketch.js
@@ -3,37 +3,52 @@ var ground, invisibleGround, groundImage;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png")
 }
 
-/*A função createCanvas() define o tamanho (em pixels) do sketch e qual o sistema de renderização utilizado*/
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(600,200);
+  
   
   //crie um sprite de trex
-  trex = createSprite(50,180,20,50);
+  trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
-  
-  //adicione dimensão e posição ao trex
   trex.scale = 0.5;
-  trex.x = 50;
   
   //crie um sprite ground (solo)
   ground = createSprite(200,180,400,20);
- 
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //crie um solo invisível
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
 }
 
 function draw() {
+  //definir cor de fundo
   background(220);
- 
-  //pular quando a tecla espaço for pressionada
-  if(keyDown("space")) {
+  
+  console.log(trex.y)
+  
+  //pular quando tecla espaço for pressionada
+  if(keyDown("space") && trex.y >= 100) {
     trex.velocityY = -10;
   }
   
+  //adicionar gravidade
   trex.velocityY = trex.velocityY + 0.8
   
- 
- //impedir que o trex caia 
-  trex.collide(ground);
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //impedir que o trex caia
+  trex.collide(invisibleGround);
+  
   drawSprites();
 }
