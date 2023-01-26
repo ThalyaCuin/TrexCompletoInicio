60806ce03bdff7ae0ffc4742fd60b566af0b8fa9
ThalyaCuin <thalyacuin@gmail.com>
2023-01-26 15:55:54 -0300

    aula13

--- sketch.js
+++ sketch.js
@@ -1,54 +1,77 @@
 var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
+var cloud,cloudsGroup,cloudImage;
+
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png")
+  trex_running = loadAnimation ("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png");
+
+  cloudImage = loadImage("cloud.png");
 }
 
-function setup() {
+function setup(){
   createCanvas(600,200);
-  
-  
-  //crie um sprite de trex
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.scale = 0.5;
-  
-  //crie um sprite ground (solo)
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -4;
-  
-  //crie um solo invisível
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
+
+  // criar um sprite trex
+
+trex = createSprite(50,180,20,50);
+trex.addAnimation("running",trex_running);
+
+//adicionar dimensão a posição trex
+
+trex.scale = 0.5;
+trex.x= 50;
+
+//criar um sprite (solo)
+
+ground = createSprite(200,180,400,20);
+ground.addImage("ground",groundImage);
+ground.x = ground.width/2;
+
+// criar um solo invisivel 
+
+invisibleGround = createSprite(200,190,400,10);
+invisibleGround.visible = false;
+
 }
 
-function draw() {
-  //definir cor de fundo
+function draw(){
   background(220);
-  
-  console.log(trex.y)
-  
-  //pular quando tecla espaço for pressionada
-  if(keyDown("space") && trex.y >= 100) {
-    trex.velocityY = -10;
-  }
-  
-  //adicionar gravidade
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
+
+  ground.velocityX= -2;
+
+  if(ground.x<0){
     ground.x = ground.width/2;
   }
   
-  //impedir que o trex caia
-  trex.collide(invisibleGround);
-  
-  drawSprites();
+  // pular quando a tecla espaço for pressionada 
+
+if (keyDown("space") && trex.y >=100){   
+  trex.velocityY= -10;
+}
+
+
+trex.velocityY = trex.velocityY + 0.8;
+
+//impedir que o meu trex caia 
+trex.collide(invisibleGround);
+
+spawClouds();
+
+drawSprites();
+
+}
+
+function spawClouds(){
+
+  if(frameCount % 60 === 0){
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage);
+    cloud.y = Math.round(random(10,60));
+    cloud.scale= 0.4;
+    cloud.velocityX= -3;
+  }
+
+ 
 }
